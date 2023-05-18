8f13d9b364e21adcb3b63266f83b32b4ad53715f
Jobega <37122818+JosephPobega@users.noreply.github.com>
2023-05-17 20:11:31 -0400

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    
+    $to = "joepobega@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+    
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for your message. We'll be in touch shortly!";
+    } else {
+        echo "Oops! Something went wrong. Please try again.";
+    }
+}
+?>
